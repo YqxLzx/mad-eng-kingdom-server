cc9cc739d9112e672c6b67f2a4e7c6d5c297c185
YqxLzx <958986887@qq.com>
2024-05-07 14:54:39 +0800

    chore: rewrite ws

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -3,21 +3,69 @@ import {
   WebSocketGateway,
   WebSocketServer,
   WsResponse,
+  OnGatewayConnection,
+  OnGatewayDisconnect
 } from "@nestjs/websockets"
 import { from, Observable } from "rxjs"
 import { map } from "rxjs/operators"
-import { Server } from "ws"
+//import { Server, Socket } from "ws"
+import { Server, Socket } from 'socket.io';
+
+import { v4 as uuidv4 } from 'uuid';
+
+interface IRoom {
+  id: string;
+  users: string[]; // 如果已匹配，则存储房间名  
+}
 
 @WebSocketGateway(8080)
-export class EventsGateway {
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server: Server
 
-  @SubscribeMessage("events")
-  onEvent(client: any, data: any): Observable<WsResponse<number>> {
-    console.log(data)
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: "events", data: item })),
-    )
+  rooms: IRoom[] = [];
+
+  handleConnection(client: Socket) {
+    // Check if there are available rooms
+    const availableRoom = this.rooms.find(room => room.users.length < 2);
+
+    if (availableRoom) {
+      // Add user to existing room
+      availableRoom.users.push(client.id);
+      client.join(availableRoom.id);
+      // Notify user about the room they joined
+      client.emit('roomJoined', { roomId: client.rooms[1] });
+    } else {
+      // Create new room
+      const newRoom: IRoom = {
+        id: uuidv4(),
+        users: [client.id],
+      };
+      this.rooms.push(newRoom);
+      client.join(newRoom.id);
+      // Notify user about the room they joined
+      client.emit('roomCreated', { roomId: client.rooms[1] });
+    }
+
+
+  }
+
+  handleDisconnect(client: Socket) {
+    // Remove user from the room
+    this.rooms.forEach(room => {
+      const index = room.users.indexOf(client.id);
+      if (index !== -1) {
+        room.users.splice(index, 1);
+        client.leave(room.id);
+        // Notify other users in the room about the user leaving
+        client.to(room.id).emit('userLeft', { userId: client.id });
+      }
+    });
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: any) {
+    // Handle messages from clients
+    // You can implement message handling logic here
   }
 }
