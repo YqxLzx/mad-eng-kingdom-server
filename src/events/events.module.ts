c89ee613fd28064512bddeacd0d9835ebf7b721a
YqxLzx <958986887@qq.com>
2024-05-13 22:34:49 +0800

    fix: 修复线上路径

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -3,6 +3,6 @@ import { EventsGateway } from "./events.gateway"
 import { WordsService } from "src/words/words.service"
 
 @Module({
-  providers: [EventsGateway,WordsService],
+  providers: [EventsGateway, WordsService],
 })
 export class EventsModule {}
